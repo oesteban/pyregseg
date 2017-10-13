245b153089212756645d1e2d90b6c93359f5af8c
oesteban <code@oscaresteban.es>
2017-10-13 16:03:14 -0700

    remove copyright header

--- regseg/misc.py
+++ regseg/misc.py
@@ -3,12 +3,8 @@
 # emacs: -*- mode: python; py-indent-offset: 4; indent-tabs-mode: nil -*-
 # vi: set ft=python sts=4 ts=4 sw=4 et:
 """
-misc.py - Miscelaneous helpers for RegSeg
+Miscelaneous helpers
 
-Copyright (c) 2013, code@oscaresteban.es (Oscar Esteban),
-                    with Biomedical Image Technology, UPM (BIT-UPM)
-All rights reserved.
-This file is part of RegSeg.
 
 """
 from __future__ import print_function, division, absolute_import, unicode_literals
